bcddeb7081f87efb3e7db594ff1f15f3b48ca367
Jen Basch <jbasch@apple.com>
2026-09-10 10:22:25 -0700

    Remove FunctionType, fold into ClassType

--- pkl-core/src/main/java/org/pkl/core/ast/type/TypeNode.java
+++ pkl-core/src/main/java/org/pkl/core/ast/type/TypeNode.java
@@ -1680,8 +1680,10 @@ public abstract class TypeNode extends PklNode {
 
     @Override
     protected VmType doGetType() {
-      return new VmType.FunctionType(
-          TypeNode.toTypes(parameterTypeNodes), returnTypeNode.getType());
+      var typeArguments = toTypes(parameterTypeNodes, parameterTypeNodes.length + 1);
+      typeArguments[parameterTypeNodes.length] = returnTypeNode.getType();
+      return new VmType.ClassType(
+          BaseModule.getFunctionNClass(parameterTypeNodes.length), typeArguments);
     }
 
     @Override
@@ -1703,7 +1705,7 @@ public abstract class TypeNode extends PklNode {
     }
 
     protected VmClass getVmClass() {
-      return ((VmType.FunctionType) getType()).getVmClass();
+      return ((VmType.ClassType) getType()).getVmClass();
     }
 
     @SuppressWarnings("unused")
@@ -1715,7 +1717,7 @@ public abstract class TypeNode extends PklNode {
 
     @Fallback
     protected Object fallback(Object value) {
-      throw typeMismatch(value, ((VmType.FunctionType) getType()).getVmClass());
+      throw typeMismatch(value, ((VmType.ClassType) getType()).getVmClass());
     }
   }
 
@@ -1765,9 +1767,8 @@ public abstract class TypeNode extends PklNode {
 
     @Override
     protected VmType doGetType() {
-      return new VmType.FunctionType(
-          toTypes(typeArgumentNodes, typeArgumentNodes.length - 1),
-          typeArgumentNodes[typeArgumentNodes.length - 1].getType());
+      return new VmType.ClassType(
+          BaseModule.getFunctionNClass(typeArgumentNodes.length - 1), toTypes(typeArgumentNodes));
     }
 
     public final VmList getTypeArgumentMirrors() {
@@ -1775,7 +1776,7 @@ public abstract class TypeNode extends PklNode {
     }
 
     protected VmClass getVmClass() {
-      return ((VmType.FunctionType) getType()).getVmClass();
+      return ((VmType.ClassType) getType()).getVmClass();
     }
 
     @SuppressWarnings("unused")
@@ -2785,10 +2786,10 @@ public abstract class TypeNode extends PklNode {
     return toTypes(typeNodes, typeNodes.length);
   }
 
-  // this variant is used for truncating maps (e.g. FunctionNTypeNode -> FunctionType)
+  // this variant is used for maps that need to change length (e.g. FunctionTypeNode -> ClassType)
   static VmType[] toTypes(TypeNode[] typeNodes, int len) {
     var result = new VmType[len];
-    for (var i = 0; i < len; i++) {
+    for (var i = 0; i < typeNodes.length; i++) {
       result[i] = typeNodes[i].getType();
     }
     return result;
